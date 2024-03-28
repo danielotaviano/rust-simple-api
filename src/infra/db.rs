b87c00cca0a951c2a1e0ab4abb29f9b258aa326c
daniel <danielotavianopontes@gmail.com>
2024-03-28 15:32:18 -0300

    some fixes

--- src/infra/db.rs
+++ src/infra/db.rs
@@ -4,7 +4,17 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 pub static DB_POOL: OnceCell<Pool<Postgres>> = OnceCell::new();
 
 pub async fn start_connection() -> Result<(), sqlx::Error> {
-    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    let postgres_user = std::env::var("POSTGRES_USER").expect("POSTGRES_USER must be set.");
+    let postgres_password =
+        std::env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set.");
+    let postgres_db = std::env::var("POSTGRES_DB").expect("POSTGRES_DB must be set.");
+    let postgres_port = std::env::var("POSTGRES_PORT").expect("POSTGRES_PORT must be set.");
+    let postgres_host = std::env::var("POSTGRES_HOST").expect("POSTGRES_HOST must be set.");
+
+    let url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        postgres_user, postgres_password, postgres_host, postgres_port, postgres_db
+    );
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
